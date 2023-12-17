06cab76468e6b5188441b35816ea5ff95d42d360
melikeozlen <melikeozlen@gmail.com>
2023-12-17 18:55:53 +0300

    dashboard kullanımı

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 import mochawesome from 'cypress-mochawesome-reporter/plugin';
 export default defineConfig({
+  projectId:"j6hwhz",
   component: {
     devServer: {
       framework: "react",
